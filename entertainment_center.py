b7bd987fee163e34f4fe698708f8390e0caf86a3
Elshibani Omar <amr.omar80@gmail.com>
2018-06-11 18:33:56 -0600

    Add a class

---
+++ entertainment_center.py
@@ -0,0 +1,5 @@
+import media
+
+toy_story = media.Movie("Toy Story", "A story of a boy and his toys that come to life", "https://dgeiu3fz282x5.cloudfront.net/g/l/l-140044.jpg", "https://www.youtube.com/watch?v=KYz2wyBy3kc")
+
+print(toy_story.storyline)
--- movies.py
+++
@@ -1,5 +0,0 @@
-# Things for class Movie to remember:
-#  - title
-#  - storyline
-#  - poster_image
-#  - trailer_youtube
